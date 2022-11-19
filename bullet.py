7612d4c9504a6e379a1313125d607b0aac3d1967
patchy-knowledge <115863136+patchy-knowledge@users.noreply.github.com>
2022-11-18 22:19:00 -0500

    Bullet generation func

--- bullet.py
+++ bullet.py
@@ -7,9 +7,20 @@ bulletList=[]
 def testBullet(x,y,speed,direction,radius,damage,lifetime):
     bulletList.append(bullet(x,y,speed,direction,radius,damage,lifetime))
 
-def checkCollision(character):
-    for bullet in bulletList:
-        if distance(character.x,character.y,bullet.x,bullet.y)<=character.radius+bullet.radius:
-            return True
+def checkCollision(character,bullet):
+    if distance(character.x,character.y,bullet.x,bullet.y)<=character.radius+bullet.radius:
+        bulletList.remove(bullet)
+        character.health-=bullet.damage
+        return True
     return False
+
+def clean(app):
+    for bullet in bulletList:
+        if bullet.x>app.width or bullet.x<0 or bullet.y<0 or bullet.y>app.height:
+            bulletList.remove(bullet)
+
+def pattern1(x,y,density,size,speed,damage,lifetime,offset):
+    count=180//density
+    for i in range(1,count):
+        bulletList.append(bullet(x,y,speed,i*density+offset,size,damage,lifetime))
 
--- main.py
+++ main.py
@@ -6,50 +6,56 @@ from helper import *
 def appStarted(app):
     app.timerDelay=10
     #tentative, test only!!!!
-    app.character=Player(10,"Yuyuko",app.width/2,app.height-10,10,1,1)
+    app.character=Player(10,"Yuyuko",app.width/2,app.height-10,5,1,1)
     app.isFocus=False
     testBullet(app.width/2,10,2,90,5,114514,1919810)
     
 def redrawAll(app,canvas):
-    canvas.create_oval(app.character.x-0.5*app.character.radius,app.character.y-0.5*app.character.radius,
-    app.character.x+0.5*app.character.radius,app.character.y+0.5*app.character.radius,fill="white")
+    canvas.create_oval(app.character.x-1.5*app.character.radius,app.character.y-1.5*app.character.radius,
+    app.character.x+1.5*app.character.radius,app.character.y+1.5*app.character.radius,fill="white")
+    canvas.create_text(300,50,font="Arial 26", text=app.character.health)
     for bullet in bulletList:
-        canvas.create_oval(bullet.x-0.5*bullet.radius,bullet.y-0.5*bullet.radius,
-        bullet.x+0.5*bullet.radius,bullet.y+0.5*bullet.radius,fill="yellow")
+        canvas.create_oval(bullet.x-1.5*bullet.radius,bullet.y-1.5*bullet.radius,
+        bullet.x+1.5*bullet.radius,bullet.y+1.5*bullet.radius,fill="yellow")
 
 def keyPressed(app,event):
     if event.key=="z":
         app.isFocus=not app.isFocus
-    if event.key=="Up":
+    if event.key=="Up" and app.character.y>=10:
         if app.isFocus:
             app.character.moveY(-1*app.character.speed/5)
         else:
             app.character.moveY(-1*app.character.speed)
-    if event.key=="Down":
+    if event.key=="Down" and app.character.y<=app.height-10:
         if app.isFocus:
             app.character.moveY(app.character.speed/5)
         else:
             app.character.moveY(app.character.speed)
-    if event.key=="Left":
+    if event.key=="Left" and app.character.x>=10:
         if app.isFocus:
             app.character.moveX(-1*app.character.speed/5)
         else:
             app.character.moveX(-1*app.character.speed)
-    if event.key=="Right":
+    if event.key=="Right" and app.character.x<=app.width-10:
         if app.isFocus:
             app.character.moveX(app.character.speed/5)
         else:
             app.character.moveX(app.character.speed)
+    #test bullet pattern
+    if event.key=="1":
+        pattern1(300,50,10,8,2,1,100,0)
+        pattern1(100,50,10,8,2,1,100,5)
+        pattern1(500,50,10,8,2,1,100,5)
+        
 
 def timerFired(app):
     for bullet in bulletList:
         dx,dy=polar2cart(bullet.direction,bullet.speed)
         bullet.x+=dx
         bullet.y+=dy
-        
-    if checkCollision(app.character):
-        print("collision detected")
-        bulletList.remove(bullet)
+        if checkCollision(app.character,bullet):
+            print("Collision Detected")
+    clean(app) 
 
 runApp(height=600,width=600)
     
